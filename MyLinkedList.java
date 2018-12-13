41be1c51120990e812540a38c2d1c9fcc91ae5ea
grace-mao23 <gmao00@stuy.edu>
2018-12-12 21:19:40 -0500

    Overall format copied into original file

---
+++ MyLinkedList.java
@@ -0,0 +1,19 @@
+class Node{
+ private int data;
+ private Node next,prev;
+}
+
+class MyLinkedList{
+ private int size;
+ private Node start,end;
+
+ public int size() {
+   return 0;
+ }
+ public boolean add(int value) {
+   return true;
+ }
+ public String toString() {
+   return "";
+ }
+}
